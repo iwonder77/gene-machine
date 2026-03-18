3c642a76dc117b4e516348e83daa0fb7cace0058
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2026-03-18 13:59:36 -0600

    decrease channel switch settle time, must consider switching to non-blocking approach if necessary, also removed dead constants

--- src/Config.h
+++ src/Config.h
@@ -6,15 +6,13 @@ namespace config {
 // ----- PIN DEFINITIONS -----
 constexpr uint8_t I2C_SDA = 16;
 constexpr uint8_t I2C_SCL = 17;
-//
+
 // ----- CONSTANTS -----
-constexpr uint32_t BUTTON_DEBOUNCE_MS = 250;
-constexpr uint8_t CHANNEL_SWITCH_SETTLE_MS = 5;
+constexpr uint8_t CHANNEL_SWITCH_SETTLE_MS = 1;
 
 // ----- RFID2 READER -----
 constexpr uint16_t POLL_INTERVAL_MS =
     50; // how often to poll all readers for tags (ms)
-constexpr uint8_t READER_INIT_SETTLE_MS = 10;
 constexpr uint32_t TAG_DEBOUNCE_TIME = 150; // debounce time for tag detection
 constexpr uint32_t TAG_ABSENCE_TIMEOUT =
     450; // time before considering tag removed
@@ -22,7 +20,7 @@ constexpr uint8_t TAG_PRESENCE_THRESHOLD = 3; // num fails before marking absent
 constexpr uint8_t TAG_START_PAGE = 4; // page # to read data from in tag memory
 constexpr uint8_t MAX_READ_ATTEMPTS = 5;
 
-// ----- I2C Addresses -----
+// ----- I2C ADDRESSES -----
 constexpr uint8_t MUX_ADDR = 0x70;
 constexpr uint8_t WS1850S_I2C_ADDR = 0x28;
 } // namespace config
