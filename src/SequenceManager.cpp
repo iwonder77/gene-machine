6ce2d6aa6db3874a05594864aac19c490a342d17
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2026-03-18 15:13:53 -0600

    implement lightweight version of SequenceManager that simply consumes events and outputs message when all slots are filled

---
+++ src/SequenceManager.cpp
@@ -0,0 +1,55 @@
+#include "SequenceManager.h"
+
+void SequenceManager::handleEvent(const Event &event) {
+  printEvent(event);
+  switch (event.type) {
+  case EventType::PieceIdentified:
+    slots_[event.slot] = event.pair;
+    // check if all slots have been filled
+    // mark sequence_complete_ as true here, the following check will revert it
+    // back to false if ANY of the slots are empty
+    sequence_complete_ = true;
+    for (int i = 0; i < config::NUM_READERS; i++) {
+      if (slots_[i] == gene_tag::NucleotidePair::None) {
+        sequence_complete_ = false;
+        break;
+      }
+    }
+
+    if (sequence_complete_) {
+      Serial.println("Slots filled!");
+      printSequence();
+    }
+    break;
+  case EventType::PieceRemoved:
+    slots_[event.slot] = gene_tag::NucleotidePair::None;
+    sequence_complete_ = false;
+    break;
+  default:
+    break;
+  }
+}
+
+void SequenceManager::printSequence() {
+  Serial.print("Sequence order: ");
+  for (int i = 0; i < config::NUM_READERS; i++) {
+    Serial.print(gene_tag::pairToString(slots_[i]));
+  }
+  Serial.println();
+}
+
+void SequenceManager::printEvent(const Event &event) {
+  switch (event.type) {
+  case EventType::PieceIdentified:
+    Serial.print(gene_tag::pairToString(event.pair));
+    Serial.print(" Nucleotide pair placed in slot: ");
+    Serial.println(event.slot);
+    break;
+  case EventType::PieceRemoved:
+    Serial.print("Piece removed from slot: ");
+    Serial.println(event.slot);
+    break;
+  default:
+    break;
+  }
+}
---
+++ src/SequenceManager.h
@@ -0,0 +1,26 @@
+
+#pragma once
+/**
+ * SequenceManager.h
+ *
+ * Central coordinator for executing critical exhibit logic (detecting
+ * sequences, sending udp packets, letting LED controller know what to animate,
+ * etc)
+ */
+#include <Arduino.h>
+
+#include "Config.h"
+#include "Event.h"
+#include "GeneMachineTag.h"
+
+class SequenceManager {
+public:
+  // void update();
+  void handleEvent(const Event &event);
+  void printSequence();
+  void printEvent(const Event &event);
+
+private:
+  gene_tag::NucleotidePair slots_[config::NUM_READERS] = {};
+  bool sequence_complete_ = false;
+};
